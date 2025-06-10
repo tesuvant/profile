758c93c88c0245a7d9e23f5c4b91088cae022d3d
Teemu Suvanto <teemu.suvanto_ext@bbraun.com>
2025-06-10 16:29:57 +0200

    foo

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,17 @@
 variable "location" {
   description = "Azure region location"
   type        = string
+  default = "northeurope"
 }
 
 variable "rg_name" {
   description = "Name of the resource group"
   type        = string
+  default = "profile"
 }
 
 variable "sa_name" {
   description = "Storage account name"
   type        = string
+  default = "827be54aprofile"
 }
-
