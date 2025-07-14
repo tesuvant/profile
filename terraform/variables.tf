9c414f992955568ba882a35c99baa871d3686d5f
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-07-14 15:20:32 +0200

    add m

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,3 +38,18 @@ variable "contact" {
     email    = "joe.average@foo.bar"
   }
 }
+
+variable "qj" {
+  description = ""
+  sensitive   = true
+  type = object({
+    f = string
+    w = string
+    t = number
+  })
+  default = {
+    f = "rg"
+    w = "lb"
+    t = 443
+  }
+}
